8660d8e09fd4b9b1f859f01ba577b2a2cb666749
DEVKEY <tyu_mom@yahoo.co.jp>
2025-05-25 04:38:57 +0900

    feat(tags): show post count

--- src/utils/getUniqueTags.ts
+++ src/utils/getUniqueTags.ts
@@ -5,18 +5,29 @@ import postFilter from "./postFilter";
 interface Tag {
   tag: string;
   tagName: string;
+  count: number;
 }
 
 const getUniqueTags = (posts: CollectionEntry<"blog">[]) => {
-  const tags: Tag[] = posts
+  const tagMap = new Map<string, { tagName: string; count: number }>();
+
+  posts
     .filter(postFilter)
     .flatMap(post => post.data.tags)
-    .map(tag => ({ tag: slugifyStr(tag), tagName: tag }))
-    .filter(
-      (value, index, self) =>
-        self.findIndex(tag => tag.tag === value.tag) === index
-    )
+    .forEach(tagName => {
+      const tag = slugifyStr(tagName);
+      const entry = tagMap.get(tag);
+      if (entry) {
+        entry.count += 1;
+      } else {
+        tagMap.set(tag, { tagName, count: 1 });
+      }
+    });
+
+  const tags: Tag[] = Array.from(tagMap.entries())
+    .map(([tag, { tagName, count }]) => ({ tag, tagName, count }))
     .sort((tagA, tagB) => tagA.tag.localeCompare(tagB.tag));
+
   return tags;
 };
 
