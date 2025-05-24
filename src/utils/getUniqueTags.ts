4c440cc599228defff7c510410753520d9d8f870
DEVKEY <tyu_mom@yahoo.co.jp>
2025-05-25 04:21:46 +0900

    Show tag counts on tags page

--- src/utils/getUniqueTags.ts
+++ src/utils/getUniqueTags.ts
@@ -5,19 +5,25 @@ import postFilter from "./postFilter";
 interface Tag {
   tag: string;
   tagName: string;
+  count: number;
 }
 
 const getUniqueTags = (posts: CollectionEntry<"blog">[]) => {
-  const tags: Tag[] = posts
+  const tagMap: Record<string, Tag> = {};
+
+  posts
     .filter(postFilter)
     .flatMap(post => post.data.tags)
-    .map(tag => ({ tag: slugifyStr(tag), tagName: tag }))
-    .filter(
-      (value, index, self) =>
-        self.findIndex(tag => tag.tag === value.tag) === index
-    )
-    .sort((tagA, tagB) => tagA.tag.localeCompare(tagB.tag));
-  return tags;
+    .forEach(tag => {
+      const slug = slugifyStr(tag);
+      if (tagMap[slug]) {
+        tagMap[slug].count += 1;
+      } else {
+        tagMap[slug] = { tag: slug, tagName: tag, count: 1 };
+      }
+    });
+
+  return Object.values(tagMap).sort((a, b) => a.tag.localeCompare(b.tag));
 };
 
 export default getUniqueTags;
